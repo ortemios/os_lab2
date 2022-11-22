4c8760f0cdf77de7fcf2f9fff71ca57340ed67ac
john <artyemlavrov39@gmail.com>
2022-11-22 17:48:42 +0300

    Remove extras

--- .kernel_module.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/john/Desktop/os_lab2/kernel_module.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000  --build-id  -T ./scripts/module-common.lds -o /home/john/Desktop/os_lab2/kernel_module.ko /home/john/Desktop/os_lab2/kernel_module.o /home/john/Desktop/os_lab2/kernel_module.mod.o;  true
--- .kernel_module.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/john/Desktop/os_lab2/kernel_module.mod := { echo  /home/john/Desktop/os_lab2/kernel_module.o;  echo; } > /home/john/Desktop/os_lab2/kernel_module.mod
